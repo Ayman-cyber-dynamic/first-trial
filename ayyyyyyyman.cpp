36adf59484ec6446a3790d9f0326d0276de3e2ec
Ayman-cyber-dynamic <70891692+Ayman-cyber-dynamic@users.noreply.github.com>
2020-09-07 18:49:05 +0200

    Add files via upload

---
+++ ayyyyyyyman.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout<<"\n\nGuess the "
+    <<"out put\n----\n";
+cout<<10+20-5<<endl;
+cout<<14/2<<endl;
+cout<<17-10<<endl;
+cout<<"endl";
+cout<<"\npractice makes perfect";
+return 0;
+}
