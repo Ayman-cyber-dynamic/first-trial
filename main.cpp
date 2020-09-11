04e88cb62750322ad65ad2f5415d180f3cc29131
Ayman-cyber-dynamic <70891692+Ayman-cyber-dynamic@users.noreply.github.com>
2020-09-11 20:26:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     int num1 , num2, num3;
     cin>> num1>>num2>>num3;
-    bool a= num1<num2<num3;
-    bool b= num2<num1<num3;
-    bool c=num3<num1<num2;
-    bool d=num1<num3<num2;
-    bool e=num2<num3<num1;
-    bool f=num3<num2<num1;
+    bool a= num1<num2 &&num2<num3;
+    bool b= num2<num1 &&num1<num3;
+    bool c=num3<num1&&num1<num2;
+    bool d=num1<num3&&num3<num2;
+    bool e=num2<num3&&num3<num1;
+    bool f=num3<num2&&num2<num1;
 
     if (a)
         cout << num1<<num2<<num3;
