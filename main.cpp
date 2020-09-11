9b46c37c28e32e85275dedbce442daacfeed5d75
Ayman-cyber-dynamic <70891692+Ayman-cyber-dynamic@users.noreply.github.com>
2020-09-11 20:09:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-
-
 
 int main()
 {
-	cout<<"My First Program. Helllllllo."<<endl;
+    int num1 , num2, num3;
+    cin>> num1>>num2>>num3;
+    bool a= num1<num2<num3;
+    bool b= num2<num1<num3;
+    bool c=num3<num1<num2;
+    bool d=num1<num3<num2;
+    bool e=num2<num3<num1;
+    bool f=num3<num2<num1;
 
-	return 0;
+    if (a)
+        cout << num1<<num2<<num3;
+        else if (b)
+            cout << num2<<num1<<num3;
+        else if (c)
+            cout <<num3<<num1<<num2;
+        else if (d)
+            cout << num1<<num3<<num2;
+        else if (e)
+            cout <<num2<<num3<<num1;
+        else if (f)
+           cout<< num3<<num2<<num1;
+    return 0;
 }
